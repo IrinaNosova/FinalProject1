2425fc0c09b932ddc98378b0abea3f5a7637e923
Irina Nosova <irinka_n-1982@mail.ru>
2023-02-20 19:57:07 +0500

    Program

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше или равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+using static System.Console;
+
+WriteLine("Введите массив через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+WriteLine();
+Console.WriteLine("{0}", string.Join(", ", SelectArray(array)));
+
+
+string[] SelectArray(string[] inArray)
+{
+    string[] resultArray = new string[ArraySize(inArray)];
+    int count = 0;
+    foreach (var item in inArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[count] = item;
+            count++;
+        }
+    }
+    return resultArray;
+
+}
+
+
+int ArraySize(string[] inArray) 
+{
+    int result = 0;
+    foreach (var item in inArray)
+    {
+        if (item.Length <= 3)
+            result++;
+    }
+    return result;
+}
+
+
